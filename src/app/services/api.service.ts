2fadc764d931a52ef6ee5762075cef0a096fd3aa
luizbaron <92989051+luizbaron@users.noreply.github.com>
2025-05-23 02:34:09 +0200

    Update API URL in ApiService to production endpoint

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class ApiService {
-  private apiUrl = 'http://localhost:8080'
+  private apiUrl = 'https://diariodebordodigital-production.up.railway.app';
 
   constructor(private http: HttpClient) {}
 
